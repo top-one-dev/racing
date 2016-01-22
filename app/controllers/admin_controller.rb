064b527eacdd627fae63ab0d823dad1d7d67edf4
Louis Dunn <louisdunn51@gmail.com>
2016-01-22 06:01:06 -0800

    fix nav

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -9,7 +9,8 @@ class AdminController < ApplicationController
   	user = AdminUser.authenticate(params[:username], params[:password])  	
   	if user
   	  session[:admin_user] = true
-  	  redirect_to admin_path, :notice => "Admin user has logged in successfully!"      
+  	  #redirect_to admin_path, :notice => "Admin user has logged in successfully!"      
+      redirect_to races_path, :notice => "Admin user has logged in successfully!"      
   	else
   	  session[:admin_user] = false
         flash[:danger] = "Invalid username or password"
