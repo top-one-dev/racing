805bcdd6ca25fdd1fd89a2431320ce6bdc8fe36e
Louis Dunn <louisdunn51@gmail.com>
2016-03-14 03:39:50 -0700

    update stage_effort before save

--- app/models/stage_effort.rb
+++ app/models/stage_effort.rb
@@ -29,10 +29,13 @@ class StageEffort < ActiveRecord::Base
             self.stage.segments.each do |segment|
               elapsed_time = 0
               result_json['segment_efforts'].each do |segment_effort|
-                #print "==#{segment_effort['segment']['id']}--#{segment.strava_segment_id}=="
-                if segment_effort['segment']['id'] == segment.strava_segment_id
-                  if elapsed_time == 0 or elapsed_time > segment_effort['elapsed_time']
-                    elapsed_time = segment_effort['elapsed_time']
+                start_date = Date.parse(segment_effort["start_date"])
+                if self.stage.active_date <= start_date and self.stage.close_date >= start_date
+                  #print "==#{segment_effort['segment']['id']}--#{segment.strava_segment_id}=="
+                  if segment_effort['segment']['id'] == segment.strava_segment_id
+                    if elapsed_time == 0 or elapsed_time > segment_effort['elapsed_time']
+                      elapsed_time = segment_effort['elapsed_time']
+                    end
                   end
                 end
               end
