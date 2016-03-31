1db125e3b2307781f70c743a992196e0742b5dd2
Louis Dunn <louisdunn51@gmail.com>
2016-03-30 19:26:30 -0700

    update stage effort model

--- app/models/stage_effort.rb
+++ app/models/stage_effort.rb
@@ -57,6 +57,7 @@ class StageEffort < ActiveRecord::Base
                 end
               end
               self.elapsed_time += elapsed_time
+              self.segment_avg_watts = self.segment_avg_watts.to_f + segment_avg_watts.to_f
               pre_segment_strava_id = segment.strava_segment_id
               pre_elapsed_times << elapsed_time
             end
