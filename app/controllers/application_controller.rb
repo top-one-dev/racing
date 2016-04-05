d0cf94b1e74ba8290f3ee17bf52e059f2aec3a3c
Louis Dunn <louisdunn51@gmail.com>
2016-04-05 10:27:45 -0700

    improve leaderboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -60,9 +60,9 @@ class ApplicationController < ActionController::Base
     end
 
     #sorted_cyclists.sort_by!{|k| k['total_points'].to_i}.reverse!
-    sorted_cyclists.sort do |a, b|
+    sorted_cyclists.sort! do |a, b|
       [b['total_points'], a['total_time']] <=> [a['total_points'], b['total_time']]
-    end 
+    end
 
     #nil_cyclists.each {|item| sorted_cyclists << item}
     #result = []
