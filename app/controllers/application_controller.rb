9a0e5b3d1a440fffcd8d414d7a05d879035db2bf
Louis Dunn <louisdunn51@gmail.com>
2016-04-26 10:30:48 -0700

    improve points logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,12 +43,10 @@ class ApplicationController < ActionController::Base
   def sort_cyclists_race(race)
     sorted_cyclists = []
     nil_cyclists = []
-    stage_max_points = Array.new(10, 1)
-    print stage_max_points
+    stage_max_points = Array.new(race.stages.count, 1)
 
-    race.stages.each do |stage, index|
-      print "#{index}-"
-      race.cyclists.each_with_index do |cyclist|      
+    race.stages.each_with_index do |stage, index|
+      race.cyclists.each do |cyclist|      
         stage_effort = cyclist.stage_efforts.find_by(stage_id: stage)
         stage_max_points[index] = stage_max_points[index] + 1 if stage_effort
       end
