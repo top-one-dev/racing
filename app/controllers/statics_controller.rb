b80208d607b30f5704dcc75dcb8aca1a56d48669
top-one-dev <toponedev@gmail.com>
2016-10-23 03:03:06 +0800

    Request ability

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -3,4 +3,10 @@ class StaticsController < ApplicationController
   def home
   	#session[:access_token] = nil
   end
+
+  def reuqest
+  	@url = params[:url] unless params[:url].nil?
+  	@result = RestClient.get @url unless @url.nil?  	
+  end
+
 end
