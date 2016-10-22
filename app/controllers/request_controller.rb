172ea7d803447d2da5a84d4c779d4a07aa0c901d
top-one-dev <toponedev@gmail.com>
2016-10-23 05:40:18 +0800

    //

--- app/controllers/request_controller.rb
+++ app/controllers/request_controller.rb
@@ -1,11 +1,11 @@
 class RequestController < ApplicationController
 	def index
-		# if defined? params[:request][:request_url]
-	  # 	@url = params[:request][:request_url] 
-	  # 	# auth_param = "Bearer #{session[:access_token]}"
-	  # 	# unless @url.nil?	  		
-		 #  # 	@result = RestClient.get URI.decode(@url), :Authorization => auth_param		  	
-	  # 	# end
-  	# end 		
+		if defined? params[:request][:request_url]
+	  	@url = params[:request][:request_url] 
+	  	# auth_param = "Bearer #{session[:access_token]}"
+	  	# unless @url.nil?	  		
+		  # 	@result = RestClient.get URI.decode(@url), :Authorization => auth_param		  	
+	  	# end
+  		end 		
 	end
 end
