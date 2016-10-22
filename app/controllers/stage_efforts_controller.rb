b39436b238344f1fd86c17bcf503ba3ab6eb42b0
top-one-dev <toponedev@gmail.com>
2016-10-22 19:00:57 +0800

    ..

--- app/controllers/stage_efforts_controller.rb
+++ app/controllers/stage_efforts_controller.rb
@@ -86,8 +86,8 @@ class StageEffortsController < ApplicationController
 	    end
 
 		def update_points(race, stage)
-			# unless race.start_date.nil? && race.end_date.nil?
-			# 	if Date.today >= race.start_date && Date.today <= race.end_date
+			unless race.start_date.nil? && race.end_date.nil?
+			 	if Date.today >= race.start_date && Date.today <= race.end_date
 			 	    cyclists = race.cyclists
 				    cyclists = sort_cyclists_stage(cyclists, stage)      
 				    index_temp = 0
@@ -112,8 +112,8 @@ class StageEffortsController < ApplicationController
 				        end
 				      end
 				    end
-			# 	end				
-			# end			
+			 	end				
+			 end			
 		end
 
 	    # Never trust parameters from the scary internet, only allow the white list through.
