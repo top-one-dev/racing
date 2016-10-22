b9947b8b9f4f2ff1e951ad2541e417c8ef95799d
top-one-dev <toponedev@gmail.com>
2016-10-23 05:22:13 +0800

    ...

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -7,10 +7,10 @@ class StaticsController < ApplicationController
   def request
   	if defined? params[:request][:request_url]
 	  	@url = params[:request][:request_url] 
-	  	auth_param = "Bearer #{session[:access_token]}"
-	  	unless @url.nil?	  		
-		  	@result = RestClient.get URI.decode(@url), :Authorization => auth_param		  	
-	  	end
+	  	# auth_param = "Bearer #{session[:access_token]}"
+	  	# unless @url.nil?	  		
+		  # 	@result = RestClient.get URI.decode(@url), :Authorization => auth_param		  	
+	  	# end
   	end  		
   end 
 
