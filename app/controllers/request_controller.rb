5ef6874f5ccf8d556e7bf6ebf1b4a90e9208f96c
top-one-dev <toponedev@gmail.com>
2016-10-24 00:33:54 +0800

    Update json rendering...

--- app/controllers/request_controller.rb
+++ app/controllers/request_controller.rb
@@ -6,8 +6,11 @@ class RequestController < ApplicationController
 	  	 unless @url.nil?	  		
 		 	@result = RestClient.get URI.decode(@url), :Authorization => auth_param		  	
 	  	 end
-  		end
+  		end  		 		
+	end
 
-  		 		
+	def json_render json_data
+		json_data.gsub(',', ',<br>');
+		return raw(json_data)
 	end
 end
