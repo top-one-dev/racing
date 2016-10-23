40b1b51a8f14eeb6ba1081677d6bd7b7304eaaf4
top-one-dev <toponedev@gmail.com>
2016-10-24 00:43:18 +0800

    Update json data render

--- app/controllers/request_controller.rb
+++ app/controllers/request_controller.rb
@@ -4,14 +4,10 @@ class RequestController < ApplicationController
 	  	@url = params[:request][:request_url] 
 	  	auth_param = "Bearer #{session[:access_token]}"
 	  	 unless @url.nil?	  		
-		 	@result = RestClient.get URI.decode(@url), :Authorization => auth_param
-		 	# @result =  @result unless @request.nil?	  	
+		 	@result = RestClient.get URI.decode(@url), :Authorization => auth_param		
 	  	 end
   		end  		 		
 	end
 
-	def json_render json_data
-		json_data.gsub(',', ',<br>');
-		return raw(json_data)
-	end
+	
 end
