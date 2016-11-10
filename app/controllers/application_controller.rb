7450dc55103e3ad5f211e3840cc45989a0502653
top-one-dev <toponedev@gmail.com>
2016-11-09 23:07:23 -0500

    Fix....

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -165,42 +165,43 @@ class ApplicationController < ActionController::Base
 
   # Cyclist result by Tom Jean...
   def cyclist_result (cyclist, race)
-    cyclist_result = []
-    unless cyclist.stage_efforts.nil? 
+    cyclist_result = []    
       if race.nil?
-         cyclist.stage_efforts.each do |stage_effort|
-            race = "#{stage_effort.stage.race.name} - #{stage_effort.stage.name}"
-            stage = stage_effort.stage.stage_no
-            time = stage_effort.elapsed_time.to_i
-            strava_url = stage_effort.strava_activity_url
-            avg_watts = "#{stage_effort.segment_avg_watts.to_f.round(2)}&nbsp;w"
-            temp = stage_effort.segment_avg_watts.to_f / cyclist.weight.to_f unless cyclist.weight.to_f == 0
-            watts_per_k = "#{temp.to_f.round(2)}&nbsp;w/kg"
-            time_stamp = stage_effort.create_date.to_i
-            cyclist_result << { 
-                                'race'        => race,
-                                'stage'       => stage, 
+          cyclist.rosters.each do |roster|
+            race = Race.find(roster.race_id)
+            race.stages.each do |stage|
+              stage_effort = cyclist.stage_efforts.find_by(stage_id: stage.id)
+              race_name = "#{race.name} - #{stage.name}"
+              time = stage_effort ? stage_effort.elapsed_time.to_i : 'DNF'
+              strava_url = stage_effort ? stage_effort.strava_activity_url : 'DNF'
+              avg_watts = stage_effort ? "#{stage_effort.segment_avg_watts.to_f.round(2)}&nbsp;w" : 'DNF'
+              temp = stage_effort ? stage_effort.segment_avg_watts.to_f / cyclist.weight.to_f unless cyclist.weight.to_f == 0 : nil
+              watts_per_k = temp ? "#{temp.to_f.round(2)}&nbsp;w/kg" : 'DNF'
+              time_stamp = stage_effort ? stage_effort.create_date.to_i : 'DNF'
+              cyclist_result << { 
+                                'race'        => race_name,
+                                'stage'       => stage.stage_no, 
                                 'time'        => time, 
                                 'strava_url'  => strava_url, 
                                 'avg_watts'   => avg_watts, 
                                 'watts_per_k' => watts_per_k, 
                                 'time_stamp'  => time_stamp
                               }
-         end       
+            end
+          end            
       else
         race.stages.each_with_index do |stage, index|
           stage_effort = cyclist.stage_efforts.find_by(stage_id: stage.id)
-          race = "#{race} - #{stage}"
-          stage = stage.stage_no
-          time = stage_effort.elapsed_time.to_i if stage_effort
-          strava_url = stage_effort.strava_activity_url if stage_effort
-          avg_watts = "#{stage_effort.segment_avg_watts.to_f.round(2)}&nbsp;w" if stage_effort
-          temp = stage_effort.segment_avg_watts.to_f / cyclist.weight.to_f unless cyclist.weight.to_f == 0
-          watts_per_k = "#{temp.to_f.round(2)}&nbsp;w/kg" if stage_effort
-          time_stamp = stage_effort.create_date.to_i if stage_effort
+          race_name = "#{race.name} - #{stage.name}"
+          time = stage_effort ? stage_effort.elapsed_time.to_i : 'DNF'
+          strava_url = stage_effort ? stage_effort.strava_activity_url : 'DNF'
+          avg_watts = stage_effort ? "#{stage_effort.segment_avg_watts.to_f.round(2)}&nbsp;w" : 'DNF'
+          temp = stage_effort ? stage_effort.segment_avg_watts.to_f / cyclist.weight.to_f unless cyclist.weight.to_f == 0 : nil
+          watts_per_k = temp ? "#{temp.to_f.round(2)}&nbsp;w/kg" : 'DNF'
+          time_stamp = stage_effort ? stage_effort.create_date.to_i : 'DNF'
           cyclist_result << { 
-                              'race'        => race,
-                              'stage'       => stage, 
+                              'race'        => race_name,
+                              'stage'       => stage.stage_no, 
                               'time'        => time, 
                               'strava_url'  => strava_url, 
                               'avg_watts'   => avg_watts, 
@@ -209,9 +210,7 @@ class ApplicationController < ActionController::Base
                             }
         end
       end
-    else
-      cyclist_result = nil
-    end
+    
     return cyclist_result
   end
 
