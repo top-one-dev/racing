c8ba7d1c9f2283965f9dbdf0d71022cbdb2ca65e
top-one-dev <toponedev@gmail.com>
2016-11-11 12:53:23 -0500

    Debug..

--- app/controllers/rosters_controller.rb
+++ app/controllers/rosters_controller.rb
@@ -43,10 +43,9 @@ class RostersController < ApplicationController
     def set_roster      
       @race = Race.find(params[:race_id]) if params[:race_id].present?
       @race = Race.find(params[:roster][:race_id]) unless params[:race_id].present?
+      puts "#{@race.name} - #{params[:race_id]} - #{params[:id]}"
       if params[:id].present?
-        unless @roster = @race.rosters.find(params[:id])
-          @roster = Roster.find(params[:id]).where(:race_id => @race.id)
-        end
+        @roster = @race.rosters.find(params[:id]) ? @race.rosters.find(params[:id]) : Roster.find(params[:id]).where(:race_id => @race.id)
       end
     end
 
