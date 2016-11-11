d99148bede79f4d415a66c8b30ece7f9d76fec7e
top-one-dev <toponedev@gmail.com>
2016-11-11 15:18:13 -0500

    Descending sort by timestamp.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -191,8 +191,8 @@ class ApplicationController < ActionController::Base
                                 'time_stamp'  => time_stamp
                               }
             end
-          end
-          cyclist_result.sort_by{|a| a['time_stamp']}.reverse!           
+          end          
+          cyclist_result.reverse           
       else
         race.stages.each do |stage|
           stage_effort = cyclist.stage_efforts.find_by(stage_id: stage.id)
