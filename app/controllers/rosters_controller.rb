afcb8dfeb94d84babb11dbde96f7a2f18027253d
top-one-dev <toponedev@gmail.com>
2016-11-21 12:06:15 -0500

    fix..

--- app/controllers/rosters_controller.rb
+++ app/controllers/rosters_controller.rb
@@ -4,7 +4,7 @@ class RostersController < ApplicationController
   # GET /rosters
   # GET /rosters.json
   def index
-    @rosters = @race.rosters.order('name DESC').reverse_order!    
+    @rosters = @race.rosters.order('cyclist.name DESC').reverse_order!    
   end
 
   # POST /rosters
